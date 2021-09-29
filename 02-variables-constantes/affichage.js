3f5cd7a2582202bfab4ffd76d4c66529fe8c0b6f
Redha TALAMINE <redha.talamine@colombbus.org>
2021-09-29 13:06:42 +0200

    maj

---
+++ 02-variables-constantes/affichage.js
@@ -0,0 +1,17 @@
+// JS pour la page affichage 
+
+// let ville = 'Nice';
+// console.log(ville);
+
+// let prixLogement = 1200;
+// console.log(prixLogement);
+
+// alert(prixLogement);
+// alert('Bienvenue chez les ch\'tis !');
+
+// let villeNaissance;
+// console.log('villeNaissance');
+villeNaissance = prompt('Bonjour, où êtes vous né ?');
+
+// alert('villeNaissance');
+alert('Vous êtes né à ' + villeNaissance);
