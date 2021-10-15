1e69d85842a2b14205b9408880ac05499fe84688
Redha TALAMINE <redha.talamine@colombbus.org>
2021-10-15 09:35:48 +0200

    exo maison js

---
+++ cours_JAVASCRIPT_suresnes_21/cours_JAVASCRIPT_suresnes_21/00_exos/exo_maison.js
@@ -0,0 +1,6 @@
+alert('Exo Maison Redha');
+
+let oran = 62;
+console.log(oran);
+
+document.write('Youb a aujourd\'hui ' + oran + ' ans');
--- cours_JAVASCRIPT_suresnes_21/cours_JAVASCRIPT_suresnes_21/00_exos/tva.js
+++ cours_JAVASCRIPT_suresnes_21/cours_JAVASCRIPT_suresnes_21/00_exos/tva.js
@@ -1,20 +1,20 @@
 // alert('Prix HT');
 
-// let prix = prompt('prix HT');
-// let ht = (prix);
-// let tva = ht * 1.20;
-// console.log(tva);
-// document.write('Résultat : Prix ' + ht + ' HT pour un total de '+ tva +' pour obtenir le prix TTC');
-// // console.log("Résultat : le prix TTC est de ' + tva + ' ");
+let prix = prompt('prix HT');
+let ht = (prix);
+let tva = ht * 1.20;
+console.log(tva);
+document.write('Résultat : Prix ' + ht + ' HT pour un total de '+ tva +' pour obtenir le prix TTC');
+// console.log("Résultat : le prix TTC est de ' + tva + ' ");
 
 //Correction
 
-let prixHT = prompt('Prix Hors Taxes :');
-console.log(prixHT);
+// let prixHT = prompt('Prix Hors Taxes :');
+// console.log(prixHT);
 
-// console.log("Le prix TTC est de : " + (prixHT * 1.2 + ' €. '));
+// // console.log("Le prix TTC est de : " + (prixHT * 1.2 + ' €. '));
 
-let prixTTC = prixHT * 1.2;
-console.log('Le prix TTC est de : ' + prixTTC + '€');
+// let prixTTC = prixHT * 1.2;
+// console.log('Le prix TTC est de : ' + prixTTC + '€');
 
-document.getElementById('p2').innerHTML = ('Pour un achat HT de ' + prixHT + ' €, le prix TTC est de : ' + prixTTC + '€');
+// document.getElementById('p2').innerHTML = ('Pour un achat HT de ' + prixHT + ' €, le prix TTC est de : ' + prixTTC + '€');
