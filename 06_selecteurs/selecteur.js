d3b15b6f6d7ead8a2b42e800ce8c2333d1834c58
Redha TALAMINE <87656324+Rdh92@users.noreply.github.com>
2021-10-18 16:50:33 +0200

    suite cours JS

---
+++ 06_selecteurs/selecteur.js
@@ -0,0 +1,7 @@
+//page sur les sélecteurs
+
+// avec cet évènement window.onload toute la pageé est chargée et cela permet de mettre le script dans head sans async ou defer
+window.onload = function(){
+    var select1 = titreN1;
+    console.info(select1);
+}
