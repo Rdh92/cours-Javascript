a126d023d4c11804cc61b89c4c8e1cb201cdf231
Redha TALAMINE <87656324+Rdh92@users.noreply.github.com>
2021-11-22 17:00:47 +0100

    exos js triangle et sapin

---
+++ cours_JAVASCRIPT_suresnes_21/cours_JAVASCRIPT_suresnes_21/00_exos/triangle.js
@@ -0,0 +1,32 @@
+// Feuille Javascript pour l'exerice 19-triangle.html 
+
+// avec une boucle For 
+function triangle(length) {
+    console.log('1 / le triangle avec une boucle for')
+    var line = "";
+    for (var i = 1; i <= length; i++) {
+      line+="#".repeat(i)+"\n";
+    }
+    return line;
+  }
+  console.log(triangle(9));
+//   fin boucle for 
+
+
+// avec une boucle while 
+console.log('2 / le triangle avec une boucle while')
+
+// fin boucle while 
+
+
+// sapin de noël
+
+
+let x = 0;
+
+if(x > 1){
+    console.log(x(3));
+    line+="#".repeat(0)+"\n";
+}else{
+    console.log(x(1));
+}
